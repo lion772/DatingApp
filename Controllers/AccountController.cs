27a2101112db620f03c984a2bd4f351ca57d4f56
william <william.steinkedemello@gmail.com>
2022-10-27 16:14:22 +0200

    add login endpoint

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -22,13 +22,36 @@ namespace API.Controllers
             using var hmac = new HMACSHA512(); //When we're finished with this class, it'll be disposed correctly
             var user = new AppUser{
                 UserName = registerDto.Username.ToLower(),
+                //Hashing password
                 PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                //Generating PasswordSalt to be stored along with hashed password into DB
                 PasswordSalt = hmac.Key
             };
              _context.Users.Add(user);
              await _context.SaveChangesAsync();
 
              return user;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            //Retrieve user from Database
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+
+            if(user == null) return Unauthorized("Invalid username");
+
+            //Using the password salt stored in DB to convert the entered user's password into hash
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            //Comparing if the hashed passwords match
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+            }
+
+            return user;
         }
 
         private async Task<bool> UserExists(string username)
