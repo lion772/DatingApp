cda81226e3300954123f75010b50d7aa35780866
william <william.steinkedemello@gmail.com>
2022-12-02 16:50:38 +0100

    fixing onSubmitHandler

--- client/src/app/nav-bar/nav-bar.component.ts
+++ client/src/app/nav-bar/nav-bar.component.ts
@@ -8,14 +8,19 @@ import { ConfigService } from '../config.service';
   styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent implements OnInit {
-  model: any = {};
+  model = {
+    username: '',
+    password: '',
+  };
 
   constructor(private httpService: ConfigService) {}
 
   ngOnInit(): void {}
 
-  async onSubmitHandler({ username, password }) {
-    console.log(username, password);
+  async onSubmitHandler() {
+    const { username, password } = this.model;
+    /* console.log(loginForm.form.controls.username.value);
+    console.log(loginForm.form.controls.password.value); */
     console.log(this.model.username, this.model.password);
     const dataReturned = await this.httpService.getUserLogin({
       username: username,
